f2f1287b6674b196aefd8f9238c553b45d00f8e1
angelovegazava20@gmail.com <angelovegazava20@gmail.com>
2025-05-01 21:36:39 -0500

    ordenamiento burbuja

--- ordenamiento2.py
+++ ordenamiento2.py
@@ -1,8 +1,8 @@
 def ordburbuja(arr):
-    for i in range(1, len(arr)):
-        for j in range(0, len(arr) - i):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    for pas in range(1, len(arr)):
+        for actual in range(0, len(arr) - pas):
+            if arr[actual] > arr[actual + 1]:
+                arr[actual], arr[actual + 1] = arr[actual + 1], arr[actual]
 
 a = [2, 8, 5, 3, 9, 4, 1]
 ordburbuja(a)
