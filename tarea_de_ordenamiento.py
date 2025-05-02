e964754a1e040c3b0f411e24cb1681e8ccae50be
angelovegazava20@gmail.com <angelovegazava20@gmail.com>
2025-05-01 21:38:33 -0500

    Tarea semana 5

--- tarea_de_ordenamiento.py
+++ tarea_de_ordenamiento.py
@@ -1,8 +1,8 @@
 def ordburbuja(arr):
-    for i in range(1, len(arr)):
-        for j in range(0, len(arr) - i):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+     for pas in range(1, len(arr)):
+        for actual in range(0, len(arr) - pas):
+            if arr[actual] > arr[actual + 1]:
+                arr[actual], arr[actual + 1] = arr[actual + 1], arr[actual]
                 
             
 def ordinsercion(arr):
@@ -19,6 +19,6 @@ ordburbuja(a)
 print("Ordenamiento burbuja ", a)
 
 
-b = [2, 8, 5, 3, 9, 4, 1]
+b = [2,8,5,3,9,4,1]
 ordinsercion(b)
 print("Ordenamiento insercion ", b)
